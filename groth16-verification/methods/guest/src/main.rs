3a544ea718274fe068b4b5313ea5a5cddcc6fe12
Rishabh Gupta <rishotics@gmail.com>
2023-12-22 14:36:25 +0530

    init commit

---
+++ groth16-verification/methods/guest/src/main.rs
@@ -0,0 +1,23 @@
+#![no_main]
+// If you want to try std support, also update the guest Cargo.toml file
+#![no_std]  // std support is experimental
+
+
+use risc0_zkvm::guest::env;
+
+use ark_groth16::{Groth16, Proof};
+
+
+risc0_zkvm::guest::entry!(main);
+
+pub fn main() {
+    // TODO: Implement your guest code here
+
+    // read the input
+    let input: u32 = env::read();
+
+    // TODO: do something with the input
+
+    // write public output to the journal
+    env::commit(&input);
+}
